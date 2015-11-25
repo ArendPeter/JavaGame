e21f63e380f8707a21200d1a4b3ad62a2c7b9ddb
Arend Peter <apc1993@gmail.com>
2015-11-25 00:38:46 -0500

    Added keyboard controls

--- src/GameController.java
+++ src/GameController.java
@@ -6,10 +6,16 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GameController extends JPanel{
+public class GameController extends JPanel implements KeyListener{
 
-	int playerX = 10;
-	int playerY = 10;
+	short playerX = 20;
+	short playerY = 20;
+	int speed = 10;
+	
+	boolean leftHeld = false;
+	boolean rightHeld = false;
+	boolean upHeld = false;
+	boolean downHeld = false;
 	
 	public static void main(String[] args)
 	{
@@ -18,15 +24,20 @@ public class GameController extends JPanel{
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
+		frame.setFocusable(true);
 		
 		GameController panel = new GameController();
 		
 		frame.add(panel);	
 		
 		frame.setVisible(true);
+
+	    panel.requestFocus();	
 	}
 	
 	public GameController(){
+		this.addKeyListener(this);
+		setFocusable(true);
 		new Thread(){
 		  public void run(){
 			try{
@@ -42,9 +53,21 @@ public class GameController extends JPanel{
 	}
 	
 	private void gameLoop(){
-		playerX += 10;
+		if(upHeld){
+			playerY-=speed;
+		}
+		if(downHeld){
+			playerY+=speed;
+		}
+		if(leftHeld){
+			playerX-=speed;
+		}
+		if(rightHeld){
+			playerX+=speed;
+		}
 		repaint();
 	}
+
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -53,4 +76,27 @@ public class GameController extends JPanel{
 		g.setColor(Color.ORANGE);
 		g.fillRect(playerX, playerY, 50, 50);
 	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	  switch(e.getKeyCode())	{
+	  case KeyEvent.VK_UP: upHeld = true; break;
+	  case KeyEvent.VK_DOWN: downHeld = true; break; 
+	  case KeyEvent.VK_LEFT: leftHeld = true; break; 
+	  case KeyEvent.VK_RIGHT: rightHeld = true; break; 
+	  }
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch(e.getKeyCode())	{
+		  case KeyEvent.VK_UP: upHeld = false; break;
+		  case KeyEvent.VK_DOWN: downHeld = false; break; 
+		  case KeyEvent.VK_LEFT: leftHeld = false; break; 
+		  case KeyEvent.VK_RIGHT: rightHeld = false; break; 
+	  }
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
 }
